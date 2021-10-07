92b64c58c41f81ccd6e81c234eb8aa5fb5d4f4ea
resat <ozkurtresat@gmail.com>
2021-10-07 16:11:20 +0200

    add properties to constructor function with .prototype

--- assignment5/5c.js
+++ assignment5/5c.js
@@ -2,17 +2,29 @@ function Student() {
     this.courseList = ['ZPR', 'TIN', 'BYT']
 }
 
-function StudentInit(id,name,surname) {
-    const student = new Student()
-    student.id = id
-    student.name = name
-    student.surname = surname
+Student.prototype.id=2
+Student.prototype.name='resat'
+Student.prototype.surname="ozkurt"
+let student2=new Student();
+student2.surname="NewSurname"
 
-    return student
-}
+// function StudentAddPrototype(id,name,surname) {
+//     const student = new Student()
+//     student.id = id
+//     student.name = name
+//     student.surname = surname
+//
+//     return student
+// }
+//
+// const student1 = new StudentInit(1,'resat','ozkurt')
+// console.log(student1.id)
+// console.log(student1.name)
+// console.log(student1.surname)
+// console.log(student1.courseList)
 
-const student1 = new StudentInit(1,'resat','ozkurt')
-console.log(student1.id)
-console.log(student1.name)
-console.log(student1.surname)
-console.log(student1.courseList)
+
+console.log(student2.id)
+console.log(student2.name)
+console.log(student2.surname)
+console.log(student2.courseList)
